00639e5592f6925cb9e00d41a84cf284dca83c9e
Chris Ducharme <ducharme.christophert@gmail.com>
2020-04-30 13:09:27 -0500

    routes and handlebars

--- db/goods_schema.sql
+++ db/goods_schema.sql
@@ -1,40 +1,36 @@
-
+  
 DROP DATABASE IF EXISTS essential_goods_db;
 CREATE DATABASE essential_goods_db;
-
 USE essential_goods_db;
 
-DROP TABLE IF EXISTS user;
-CREATE TABLE user(
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    userName VARCHAR(255) NOT NULL
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    createdAt datetime NOT NULL,
+	updatedAt datetime NOT NULL
 );
-
 DROP TABLE IF EXISTS product;
 CREATE TABLE product(
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     productName VARCHAR(255) NOT NULL,
     price DECIMAL(6,2),
     quantity INT NOT NULL,
-    notes VARCHAR(255) NOT NULL
+    servingSize VARCHAR(255) NOT NULL
 );
-
 DROP TABLE IF EXISTS locations;
 CREATE TABLE locations(
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    locationName VARCHAR(255) NOT NULL
-    locationCity VARCHAR(255) NOT NULL,
-    locationState VARCHAR(20) NOT NULL,
-    locationPhone VARCHAR(14) NOT NULL
+    locationName VARCHAR(255) NOT NULL,
+    locationStreet VARCHAR(255) NOT NULL,
+    locationCity VARCHAR(20) NOT NULL,
+    locationState VARCHAR(14) NOT NULL,
+    productID varchar (10) NOT NULL,
+    locationLink varchar(255) NOT NULL
 );
-
 DROP TABLE IF EXISTS saved_products;
 CREATE TABLE saved_products(
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL
 );
-
-
-
-
+USE essential_goods_db;
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,66 +1,60 @@
 // Requiring path to so we can use relative routes to our HTML files
-var path = require('path')
+const express = require("express");
+var path = require('path');
+const db = require("../models");
+const Sequelize = require('sequelize');
+
 
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require('../config/middleware/isAuthenticated')
 
 module.exports = app => {
-  app.get("/", (req, res) => {
-    // This route for now is testing the handlebars files\
-    search(req.query.searchValue).then(podcasts => {
-      res.render("home", { podcasts: podcasts });
+  app.get("/", function(req, res) {
+ // If the user already has an account send them to the members page
+    if (req.user) {
+    res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
     });
-  });
 
-  app.get('/login', (req, res) => {
-    // If the user already has an account send them to the members page
-    res.render("login");
-  })
+    app.get("/login", function(req, res) {
+    
+    if (req.user) {
+    res.redirect("/login");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+    });
+ 
+    app.get("/signup", function(req, res) {
+    if (req.user) {
+    res.redirect("/login");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    });
 
-  app.get("/signup", (req, res) => {
-    // This route for now is testing the handlebars files
-    res.render("signup");
-  });
+    app.get("/products", (req, res) => {
+        // This route for now is testing the handlebars files
+    
+      res.render("index");
+    });
+
 
   app.get("/products/:id", (req, res) => {
     // This route for now is testing the handlebars files
-    db.products.findOne({
-      where: {
-        id: req.params.id
-      }
-    }).then(dbProducts => {
-      const products = dbProducts.toJSON();
-      res.render("products", { products });
+      res.render("product_info");
     });
-  });
+ 
 
-  app.get("/saved_products/:id", (req, res) => {
+  app.get("/saved_products/", (req, res) => {
     // This route for now is testing the handlebars files
-    db.saved_products.findOne({
-      where: {
-        id: req.params.id
-      },
-      include: [db.products]
-    }).then(dbSavedProducts => {
-      const oneSavedProducts = dbSavedProducts.toJSON();
-      res.render("oneSavedProducts", { oneSavedProducts });
+      res.render("saved_products");
     });
-  });
 
-  app.get("/saved_products", (req, res) => {
-    // This route for now is testing the handlebars files
-    db.saved_products.findAll({
-      include: [db.user]
-    }).then(dbSavedProducts => {
-      const saved_products = dbSavedProducts.map(saved_products=> saved_products.toJSON());
-      //console.log(collections);
-      res.render("saved_products", { saved_products });
-    });
-  });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get('/members', isAuthenticated, function (req, res) {
-    res.render("members", { members });
-  })
-}
+  app.get("/members", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/members.html"));
+  });
+
+};
