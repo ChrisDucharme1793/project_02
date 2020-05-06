d810297feaf7be5ed6aafbfe83c081f0b6ba92d9
Chris Ducharme <58921765+ChrisDucharme1793@users.noreply.github.com>
2020-05-06 18:12:17 -0500

    Update saved_products.js

--- public/js/saved_products.js
+++ public/js/saved_products.js
@@ -1,23 +1,29 @@
-$(function() {
-	$(".btn-saved").on("click", function(event) {
-        let id = $(this).data("id");
-        let productName = $(this).data("");
-        const locationName = $(this).data("locationName");
-        const quantity = $(this).data("quantity");
-        const userId = 1;
-        const locationLink = $(this).data("locationLink");
-		$.ajax("/api/saved_products/", {
-			type: "POST",
-			data: {
-				 locationName: locationName,
-                 productName: productName,
-                 quantity: quantity,
-                 locationLink: locationLink,
-                 userId: userId,
-                 price: price
-			}
-		}).then(function() {
+$(function AddSavedProducts() {
+  $('.btn-saved1').on('click', function(event) {
+    
+    let id = $(this).data("id");
+    const productName = $(this).data('productName');
+    const locationName = $(this).data('locationName');
+    const quantity = $(this).data('quantity');
+    var userId = $(this).data('userId');
+    const price = $(this).data('price');
+    var locationLink = $(this).data('locationLink'); 
+    var itemImg = $(this).data('itemImg'); 
+    console.log("---------")
+    console.log(price)
+    $.ajax('/api/saved_products/', {
+      type: 'POST',
+      data: {
+        productName: productName,
+        locationName: locationName,
+        quantity: quantity,
+        locationLink: locationLink,
+        userId: userId,
+        price: price,
+        itemImg: itemImg,
+      }
+    }).then(function() {
 			location.reload();
 		});
-    });
-})
+  });
+});
